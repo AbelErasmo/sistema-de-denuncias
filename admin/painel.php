18e5a881ff58c1b2a805dd0aecf3a1e617ca1252
Abel Erasmo <erasmosibinde@gmail.com>
2025-07-03 00:19:12 +0200

    Actualizacao visual

--- admin/painel.php
+++ admin/painel.php
@@ -9,12 +9,12 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Painel Administrativo</title>
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/admin.css">
     <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
     <div class="container-login">
         <h1>Painel Administrativo</h1>
-   </div>
+    </div>
 </body>
 </html>
